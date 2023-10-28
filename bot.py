bd7ffd8520b19931e9df3f01ac0991dce6c727a8
Doronshapira <73933720+Doronshapira@users.noreply.github.com>
2023-10-28 12:13:43 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,83 @@
+import time
+import requests
+from main import initilize_bot
+from datetime import datetime
+import pytz
+
+# Replace 'YOUR_BOT_TOKEN' with the actual token you obtained from the BotFather
+TOKEN = '6864884396:AAEvwSSrNmF3prXUxMhDjPsVjw-ZlMsRHyw'
+
+# Replace 'YOUR_CHAT_ID' with the actual chat ID you want to send messages to
+CHAT_ID = '-1002097430141'
+
+
+# Variable to keep track of the maximum processed message ID
+last_message_unix = 0
+message_dict = initilize_bot()
+
+def subtract_dicts(dict1, dict2):
+    # Create a new dictionary to store the elements that didn't appear in the first dictionary
+    result_dict = {}
+    # Iterate through the items in the second dictionary
+    for key, value in dict1.items():
+        # Check if the key is not present in the first dictionary
+        if key not in dict2:
+            # Add the key-value pair to the result dictionary
+            result_dict[key] = value
+
+    return result_dict
+def send_message(message_details):
+    chat_id = CHAT_ID
+    media=message_details['Media Type']
+    if media:
+        requests.post(f'https://api.telegram.org/bot{TOKEN}/send{media}',
+                                 params={'chat_id': chat_id, media.lower(): message_details['Media URL'],
+                                         'caption': message_details['Message']})
+    else:
+        # Create the message text without the time
+        message_text = f"{message_details['Reporter Name']}\n" \
+                       f"{message_details['Message']}"
+    # Send the message to the specified chat
+        requests.post(f'https://api.telegram.org/bot{TOKEN}/sendMessage',
+                             params={'chat_id': chat_id, 'text': message_text, 'parse_mode': 'Markdown'})
+
+def msg_id_to_unix(message_details):
+    il_timezone = pytz.timezone('Israel')
+    # Get the current date and time in Israel time
+    current_time_il = datetime.now(il_timezone)
+    formatted_time = current_time_il.strftime("%Y-%m-%d")
+    time = message_details['Time']
+    # Convert the time string to a datetime object
+    datetime_object = datetime.strptime(formatted_time + ' {}'.format(time), "%Y-%m-%d %H:%M")
+    # Convert the datetime object to a Unix timestamp
+    unix_timestamp = int(datetime_object.timestamp())
+    return unix_timestamp
+def update_channel(last_dict):
+    global last_message_unix  # Use the global variable
+    # For demonstration purposes, let's update a sample message in message_dict
+    message_dict = initilize_bot()
+    result = subtract_dicts(message_dict, last_dict)
+    # Send updates to the channel for the new messages
+    for msg_id, message_details in result.items():
+        last_update=msg_id_to_unix(message_details)
+        # Check if the message has already been sent based on message ID
+        if int(last_update) > last_message_unix:
+            last_message_unix=int(last_update)
+            send_message(message_details)
+            time.sleep(1)  # To avoid rate limiting, you can adjust this as needed
+
+
+def main():
+    global last_message_unix
+    global message_dict
+    # Set up the scheduler for background task
+    while True:
+        try:
+            update_channel(message_dict)
+        except:
+            pass
+        time.sleep(10)  # Update every 60 seconds
+
+
+if __name__ == '__main__':
+    main()
---
+++ main.py
@@ -0,0 +1,87 @@
+import time
+
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import time
+def chrome_options():
+    chrome_options = Options()
+    chrome_options.add_argument("disable-blink-features=AutomationControlled")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--allow-insecure-localhost")
+    chrome_options.add_argument("disable-infobars")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--incognito')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('window-size=1200x600')
+    return chrome_options
+
+
+def get_media_type(media_item):
+    # Check if the media item is a video
+    if media_item and 'mc-content-media-item_video' in media_item['class']:
+        return 'Video'
+
+    # Check if the media item is an image
+    elif media_item and 'mc-content-media-item_picture' in media_item['class']:
+        return 'Photo'
+
+    # Return null if no media item or unknown type
+    return None
+
+
+# Create a dictionary to store the results
+
+def extract_info(soup):
+    # Extracting msg_id
+    msg_id = soup['data-msg-id']
+
+    # Extracting reporter name
+    reporter_name = soup.find('p', class_='mc-message-header__name').text.strip()
+
+    # Extracting message
+    message = soup.find('div', class_='mc-extendable-text__content')
+    if message:
+        message = message.text.strip()
+    else:
+        message = None
+    media_item = soup.find('div', class_='mc-content-media-item')
+    media=get_media_type(media_item)
+    if media =='Photo':
+        media_url = media_item['style'].split('("')[1].split('")')[0]
+    elif media == 'Video':
+        url=media_item['style'].split('("')[1].split('")')[0]
+        to_add=url.split('telegram')[-1].replace('thumb','video').replace('jpg','mp4')
+        media_url='https://makostorepdl-a.akamaihd.net/telegram'+to_add
+    else:
+        media_url = None
+    # Extracting message time
+    message_time = soup.find('p', class_='mc-message-footer__time').text.strip()
+
+    return {msg_id: {'Reporter Name': reporter_name, 'Message': message, 'Media Type':media,'Media URL': media_url, 'Time': message_time}}
+
+
+
+def initilize_bot():
+    output_dict = {}
+    service = Service()
+    driver = webdriver.Chrome(service=service, options=chrome_options())
+    driver.get('https://www.n12.co.il/')
+    x = WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'mc-drawer__btn')))
+    x.click()
+    html_code = driver.page_source
+    # Parse the HTML code
+    soup = BeautifulSoup(html_code, 'html.parser')
+    # Find all the message wraps
+    message_wraps = soup.find_all('div', class_='mc-message-wrap')
+    # Process each message wrap and store the results in the array
+    for message_wrap in message_wraps:
+        result = extract_info(message_wrap)
+        output_dict.update(result)
+    return output_dict
