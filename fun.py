951a2896853a5203b74e08b09b894f7c11ec87a1
Doronshapira <73933720+Doronshapira@users.noreply.github.com>
2023-10-29 19:40:03 +0200

    Update fun.py

--- fun.py
+++ fun.py
@@ -1,7 +1,9 @@
 import time
 
 # from selenium.webdriver.chrome.service import Service
-from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
@@ -69,7 +71,7 @@ def extract_info(soup):
 
 def initilize_bot():
     output_dict = {}
-    driver = webdriver.Chrome(options=chrome_options())
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.get('https://www.n12.co.il/')
     x = WebDriverWait(driver, 30).until(
         EC.presence_of_element_located((By.CLASS_NAME, 'mc-drawer__btn')))
